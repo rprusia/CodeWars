0f6780bfc818f2042a435ef155046554da14ad13
Ray Prusia <prusiarl@legal.regn.net>
2020-07-27 15:30:31 -0400

    Freecode camp - Python CERT problem 1

--- src/pythonfiles/freecodecamp/arithmic/arithmetic_arranger.py
+++ src/pythonfiles/freecodecamp/arithmic/arithmetic_arranger.py
@@ -1,5 +1,36 @@
 def arithmetic_arranger(problems):
 
-    arranged_problems = 3
 
+
+    # first put all problems into list, using split
+    for problem in problems:
+
+        if (len(problems) > 5):
+            return 'Error: Too many problems.'
+
+        currentList = problem.split(' ')
+        if (not(str(currentList[0]).isdigit() and str(currentList[2]).isdigit())):
+
+            return 'Error: Numbers must only contain digits'
+
+        if not(currentList[1] == '+' or currentList[1] == '-'):
+            return "Error: Operator must be '+' or '-' ."
+
+        if (currentList[1] == '-'):
+            currentList.append(int(currentList[0]) - int(currentList[2]))
+        elif (currentList[1] == '+'):
+            currentList.append(int(currentList[0]) + int(currentList[2]))
+
+    # for problem in v:
+    #
+    #     if (problem.isdigit())
+    #         listOfProblems = problem.split(' ')
+    #         print (listOfProblems[0])
+    #         print (listOfProblems[1] + ' ' + listOfProblems[2])
+    #         print ('_____' )
+    #         print ( calc(listOfProblems[0], listOfProblems[2], listOfProblems[1]))
+    #         print('\n')
+
+
+    arranged_problems = ' asldkfj'
     return arranged_problems
--- src/pythonfiles/freecodecamp/arithmic/main.py
+++ src/pythonfiles/freecodecamp/arithmic/main.py
@@ -3,33 +3,8 @@ from pythonfiles.freecodecamp.arithmic.arithmetic_arranger import arithmetic_arr
 from unittest import main
 
 
-#print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
-def calc(val1, val2, math):
-
-    if (math == '-'):
-        return int(val1)- int(val2)
-    elif (math == '+'):
-        return int(val1)+ int(val2)
-    elif (math == '/'):
-        return int(val1)  / int(val2)
-    elif (math == '*'):
-        return int(val1) * int(val2)
-
-v = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-
-x = v[0].split(' ')
-
-if (x[0] > x[2]):
-    print (x[0])
-    print (x[1] + ' ' + x[2])
-    print ('____' )
-    print ( calc(x[0], x[2], x[1]))
-elif (x[2] > x[0]):
-    print (' ' + x[2])
-    print (x[1] + ' ' + x[0])
-    print ('____')
-    print ( calc(x[0], x[2], x[1]))
 
 
 # Run unit tests automatically
